8ddf05ea7a38a6e82d1ba70d21fd93e335683010
Konstantin Goncharov <konstantin.goncharov@inplat.ru>
2019-05-21 19:04:41 +0300

    chore:   - separate test script into vagrantable and vagrant-free

--- check_vagrant.sh
+++ check_vagrant.sh
@@ -1,3 +1,5 @@
+vagrant up
+
 ansible testserver -m ping
 ansible testserver -m command -a uptime
 ansible testserver -a "tail /var/log/auth.log" --become
@@ -6,4 +8,6 @@ curl 127.0.0.1:18080
 ansible-playbook playbooks/-01.remove_nginx.yml
 curl 127.0.0.1:18080
 
+vagrant destroy -f
+yes | rm playbooks/*.retry
 
